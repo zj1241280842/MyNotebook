a3069b389ee74d599ca217294550ed87586100eb
zj1241280842 <94829586+zj1241280842@users.noreply.github.com>
2022-02-11 14:38:19 +0800

    Add files via upload

---
+++ 串口屏练习/Project/output/DefineID.h
@@ -0,0 +1,137 @@
+//创建画面主菜单的命令索引ID
+#define  _SCREEN_主菜单                                                           0
+
+//创建画面删除界面的命令索引ID
+#define  _SCREEN_删除界面                                                          1
+
+//创建画面实验界面的命令索引ID
+#define  _SCREEN_实验界面                                                          2
+
+//创建画面增加界面的命令索引ID
+#define  _SCREEN_增加界面                                                          3
+
+//创建画面设置1界面的命令索引ID
+#define  _SCREEN_设置1界面                                                         4
+
+//创建画面循环-删除界面的命令索引ID
+#define  _SCREEN_循环-删除界面                                                       5
+
+//创建画面循环-增加界面的命令索引ID
+#define  _SCREEN_循环-增加界面                                                       6
+
+//创建画面实验数据界面的命令索引ID
+#define  _SCREEN_实验数据界面                                                        7
+
+#define  _BTN_主菜单_BUTTON1                                                      1
+
+//全局图片主界面选中.png
+#define  _IMG_主界面选中_PNG                                                        1
+
+#define  _BTN_主菜单_BUTTON2                                                      2
+
+#define  _BTN_删除界面_BUTTON1                                                     1
+
+//全局图片删除界面-选中.png
+#define  _IMG_删除界面-选中_PNG                                                      3
+
+#define  _BTN_删除界面_BUTTON2                                                     2
+
+#define  _TXT_DIS__删除界面_TEXT_DISPLAY1                                          3
+
+#define  _GRAPH_实验界面_GRAPH1                                                    1
+
+#define  _BTN_实验界面_BUTTON1                                                     2
+
+#define  _BTN_增加界面_BUTTON1                                                     1
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY1                                          3
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY2                                          5
+
+#define  _BTN_增加界面_BUTTON3                                                     6
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY3                                          7
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY4                                          8
+
+#define  _MENU_增加界面_MENU2                                                     11
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY6                                          9
+
+#define  _BTN_增加界面_BUTTON4                                                    12
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY7                                         13
+
+#define  _MENU_增加界面_MENU1                                                      4
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY8                                         14
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY9                                         15
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY10                                        10
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY5                                         16
+
+//画面增加界面中图片Image2使用的图片
+#define  _IMG_增加界面_IMAGE2                                                      7
+
+//全局图片增加界面-选中.png
+#define  _IMG_增加界面-选中_PNG                                                      6
+
+#define  _BTN_增加界面_BUTTON2                                                     2
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY11                                        19
+
+#define  _TXT_DIS__增加界面_TEXT_DISPLAY12                                        20
+
+#define  _BTN_设置1界面_BUTTON1                                                    1
+
+#define  _RECORD_设置1界面_RECORD1                                                 2
+
+//全局图片新设置界面-选中.png
+#define  _IMG_新设置界面-选中_PNG                                                     9
+
+#define  _BTN_设置1界面_BUTTON2                                                    3
+
+#define  _BTN_设置1界面_BUTTON3                                                    4
+
+#define  _RECORD_设置1界面_RECORD2                                                 5
+
+#define  _BTN_设置1界面_BUTTON4                                                    6
+
+#define  _BTN_设置1界面_BUTTON5                                                    7
+
+#define  _BTN_设置1界面_BUTTON6                                                    8
+
+#define  _BTN_设置1界面_BUTTON7                                                    9
+
+//画面循环-删除界面的背景图片
+#define  _IMG_循环-删除界面                                                          2
+
+#define  _BTN_循环-删除界面_BUTTON1                                                  1
+
+#define  _BTN_循环-删除界面_BUTTON2                                                  2
+
+#define  _TXT_DIS__循环-删除界面_TEXT_DISPLAY1                                       3
+
+#define  _TXT_DIS__循环-增加界面_TEXT_DISPLAY1                                       1
+
+#define  _TXT_DIS__循环-增加界面_TEXT_DISPLAY2                                       2
+
+#define  _TXT_DIS__循环-增加界面_TEXT_DISPLAY6                                       4
+
+#define  _TXT_DIS__循环-增加界面_TEXT_DISPLAY10                                      3
+
+#define  _BTN_循环-增加界面_BUTTON1                                                  5
+
+//全局图片循环-增加界面-选中.png
+#define  _IMG_循环-增加界面-选中_PNG                                                  11
+
+#define  _BTN_循环-增加界面_BUTTON2                                                  6
+
+#define  _GRAPH_实验数据界面_GRAPH1                                                  1
+
+#define  _GRAPH_实验数据界面_GRAPH2                                                  2
+
+#define  _BTN_实验数据界面_BUTTON1                                                   3
+
