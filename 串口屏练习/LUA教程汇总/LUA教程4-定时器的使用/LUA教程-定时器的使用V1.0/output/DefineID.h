7d94528c91a206a759cd495fd7099b55dd2dbeb4
zj1241280842 <94829586+zj1241280842@users.noreply.github.com>
2022-02-11 14:56:48 +0800

    Add files via upload

---
+++ 串口屏练习/LUA教程汇总/LUA教程4-定时器的使用/LUA教程-定时器的使用V1.0/output/DefineID.h
@@ -0,0 +1,12 @@
+//创建画面Screen0的命令索引ID
+#define  _SCREEN_SCREEN0                                                       0
+
+#define  _BTN_SCREEN0_BUTTON1                                                  1
+
+//全局图片35_按下.jpg
+#define  _IMG_35_按下_JPG                                                        1
+
+#define  _BTN_SCREEN0_BUTTON2                                                  2
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY7                                      15
+
