b45381da789846ec4894ec73561f1b2cc2fca767
zj1241280842 <94829586+zj1241280842@users.noreply.github.com>
2022-02-11 15:00:54 +0800

    Add files via upload

---
+++ 串口屏练习/LUA教程汇总/LUA教程6-运算和字符串处理/工程文件-运算和字符串处理V1.0/output/DefineID.h
@@ -0,0 +1,123 @@
+//创建画面主页的命令索引ID
+#define  _SCREEN_主页                                                            0
+
+//创建画面简单运算的命令索引ID
+#define  _SCREEN_简单运算                                                          1
+
+//创建画面math库的命令索引ID
+#define  _SCREEN_MATH库                                                         2
+
+//创建画面字符串处理的命令索引ID
+#define  _SCREEN_字符串处理                                                         3
+
+//创建画面简单的逻辑处理的命令索引ID
+#define  _SCREEN_简单的逻辑处理                                                       4
+
+//创建画面提示的命令索引ID
+#define  _SCREEN_提示                                                            5
+
+#define  _BTN_主页_BUTTON1                                                       1
+
+//全局图片28_按下.jpg
+#define  _IMG_28_按下_JPG                                                        1
+
+#define  _BTN_主页_BUTTON2                                                       2
+
+#define  _BTN_主页_BUTTON3                                                       3
+
+#define  _BTN_主页_BUTTON4                                                       4
+
+#define  _BTN_简单运算_BUTTON3                                                    80
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY7                                          1
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY9                                          2
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY10                                         3
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY11                                         4
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY12                                         5
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY13                                         6
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY22                                         7
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY23                                         8
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY24                                         9
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY25                                        10
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY26                                        11
+
+#define  _TXT_DIS__简单运算_TEXT_DISPLAY27                                        12
+
+//全局图片29_按下.jpg
+#define  _IMG_29_按下_JPG                                                        3
+
+#define  _BTN_简单运算_BUTTON1                                                    13
+
+#define  _BTN_MATH库_BUTTON1                                                   80
+
+#define  _TXT_DIS__MATH库_TEXT_DISPLAY8                                         2
+
+#define  _TXT_DIS__MATH库_TEXT_DISPLAY9                                         3
+
+#define  _TXT_DIS__MATH库_TEXT_DISPLAY10                                        4
+
+#define  _TXT_DIS__MATH库_TEXT_DISPLAY11                                        5
+
+#define  _TXT_DIS__MATH库_TEXT_DISPLAY12                                        6
+
+#define  _TXT_DIS__MATH库_TEXT_DISPLAY13                                        7
+
+#define  _TXT_DIS__MATH库_TEXT_DISPLAY14                                        8
+
+#define  _TXT_DIS__MATH库_TEXT_DISPLAY20                                        9
+
+#define  _TXT_DIS__MATH库_TEXT_DISPLAY21                                       10
+
+//全局图片30_按下.jpg
+#define  _IMG_30_按下_JPG                                                        5
+
+#define  _BTN_MATH库_BUTTON2                                                   12
+
+#define  _TXT_DIS__MATH库_TEXT_DISPLAY22                                       13
+
+#define  _BTN_字符串处理_BUTTON1                                                   80
+
+#define  _TXT_DIS__字符串处理_TEXT_DISPLAY4                                         1
+
+#define  _TXT_DIS__字符串处理_TEXT_DISPLAY5                                         2
+
+#define  _TXT_DIS__字符串处理_TEXT_DISPLAY7                                         3
+
+#define  _TXT_DIS__字符串处理_TEXT_DISPLAY12                                        4
+
+#define  _TXT_DIS__字符串处理_TEXT_DISPLAY13                                        5
+
+#define  _TXT_DIS__字符串处理_TEXT_DISPLAY16                                        6
+
+#define  _TXT_DIS__字符串处理_TEXT_DISPLAY19                                        7
+
+//全局图片31_按下.jpg
+#define  _IMG_31_按下_JPG                                                        7
+
+#define  _BTN_字符串处理_BUTTON2                                                   12
+
+#define  _BTN_简单的逻辑处理_BUTTON1                                                  1
+
+#define  _TXT_DIS__简单的逻辑处理_TEXT_DISPLAY2                                       3
+
+#define  _BTN_简单的逻辑处理_BUTTON2                                                  2
+
+//全局图片32_按下.jpg
+#define  _IMG_32_按下_JPG                                                        9
+
+#define  _BTN_简单的逻辑处理_BUTTON3                                                  4
+
+#define  _BTN_提示_BUTTON1                                                       2
+
+#define  _BTN_提示_BUTTON2                                                       3
+
