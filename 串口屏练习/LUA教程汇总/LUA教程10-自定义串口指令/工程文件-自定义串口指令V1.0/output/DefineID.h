aa123579c8e22fcdb7a4926ade294e7ed76e059f
zj1241280842 <94829586+zj1241280842@users.noreply.github.com>
2022-02-11 15:07:08 +0800

    Add files via upload

---
+++ 串口屏练习/LUA教程汇总/LUA教程10-自定义串口指令/工程文件-自定义串口指令V1.0/output/DefineID.h
@@ -0,0 +1,16 @@
+//创建画面Screen0的命令索引ID
+#define  _SCREEN_SCREEN0                                                       0
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY7                                      26
+
+#define  _BTN_SCREEN0_BUTTON1                                                  1
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY2                                       2
+
+//全局图片33_按下.jpg
+#define  _IMG_33_按下_JPG                                                        1
+
+#define  _BTN_SCREEN0_BUTTON2                                                  4
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY4                                       5
+
